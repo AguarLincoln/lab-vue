66d63b174fb90d04bada6d059b54f22559f00398
Lincoln Aguiar <lincolnalca@gmail.com>
2021-03-11 15:54:10 -0300

    configurando ambiente

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-  <router-view></router-view>
-</div>
-
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'Home',
   components: {
-//    HelloWorld
+    HelloWorld
   }
 }
 </script>
@@ -23,7 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: black ;
   margin-top: 60px;
 }
 </style>
